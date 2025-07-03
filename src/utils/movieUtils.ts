f5c68f0451e8076091116cee77bdc375b89c2243
medinoSam <samuelejosuetutorial@gmail.com>
2025-07-03 11:55:06 -0300

    feat: implementation of getProfit function

--- src/utils/movieUtils.ts
+++ src/utils/movieUtils.ts
@@ -1,5 +1,5 @@
 import { Movie, MovieParsed } from "../types/movie";
-
+import { numeralScale } from "../constants/movieConstants";
 
 export const parsedMovie = (movieOriginal: Movie):MovieParsed => {
     const parsed: MovieParsed = {
@@ -21,5 +21,36 @@ const  convertMinutesToSeconds = (minuts:number) => {
 }
 
 const getProfit = (orcamento:string, bilheteria:string) => {
-    
+    const budgetValue = getNumberWithScale(orcamento);
+    const boxOfficeValue = getNumberWithScale(bilheteria);
+
+    if (!budgetValue || !boxOfficeValue) {
+        console.error("erro: valores de orcamento e bilheteria nao podem ser nulos");
+        return null;
+    }
+    return String(boxOfficeValue - budgetValue);
+}
+
+const getNumberValue = (value:string) => {
+    // encontra valores float
+    const matches = value.match(/[-+]?\d*\.?\d+/g);
+    if (!matches){
+        console.error("valor nulo! nao foi possivel encontrar o valor");
+        return null;
+    }
+
+    const numberValue = parseFloat(matches[0]);
+    return numberValue;
+}
+
+const  getNumberWithScale = (value:string) => {
+
+    const numberValue = getNumberValue(value);
+    const scale = numeralScale[value.split(' ')[1]]
+
+    if(!numberValue) {
+        console.error("valor nulo! nao foi possivel encontrar o valor");
+        return null;
+    }
+    return numberValue * scale;
 }
