f1fc57082cc7ab32bd6ed897b527808db19c4ed1
medinoSam <samuelejosuetutorial@gmail.com>
2025-07-04 09:35:51 -0300

    feat: create helperUtils

---
+++ src/utils/helpersUtils.ts
@@ -0,0 +1,43 @@
+import { NUMERAL_SCALE } from "../constants/movieConstants";
+
+
+export const  convertMinutesToSeconds = (minuts:number) => {
+    return minuts * 60;
+}
+
+export const  getNumberWithScale = (value:string) => {
+
+    const numberValue = getNumberValue(value);
+    const scale = NUMERAL_SCALE[value.split(' ')[1]]
+
+    if(!numberValue) {
+        console.error("valor nulo! nao foi possivel encontrar o valor");
+        return null;
+    }
+    return numberValue * scale;
+}
+
+export const getNumberValue = (value:string) => {
+    // encontra valores float
+    const matches = value.match(/[-+]?\d*\.?\d+/g);
+    if (!matches){
+        console.error("valor nulo! nao foi possivel encontrar o valor");
+        return null;
+    }
+
+    const numberValue = parseFloat(matches[0]);
+    return numberValue;
+}
+
+export const formaterNumberExtense = (value: number): string => {
+    const result = new Intl.NumberFormat("en-US", {
+        notation: "compact",
+        compactDisplay: "short",
+        maximumFractionDigits: 3
+    }).format(value);
+
+    return result
+        .replace("K", " mil")
+        .replace("M", " milhões")
+        .replace("B", " bilhões");
+}
