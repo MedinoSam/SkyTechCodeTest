32af2b45069421eee51db0439ff4fc0ba313db41
medinoSam <samuelejosuetutorial@gmail.com>
2025-07-06 17:14:53 -0300

    feat: nothing changed

--- src/routes/movieRouter.ts
+++ src/routes/movieRouter.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { getMoviesParsedController } from "../controllers/movieController";
+
+
 const movieRouter = Router();
 
 movieRouter.get('/api/filmes', getMoviesParsedController)
