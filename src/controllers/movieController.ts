264e294d3475bd8356b60f1a2b71fe0c839b3352
medinoSam <samuelejosuetutorial@gmail.com>
2025-07-06 17:14:40 -0300

    feat: filter by query params and validate request params

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -1,17 +1,22 @@
 import { Request, Response} from "express";
 import { getMoviesParsedService } from "../services/movieService";
-import { query } from "express-validator";
+import { hasGenero } from "../utils/helpersUtils";
+import { genders } from "../constants/genderConstants";
+
 
 export const getMoviesParsedController =  async (req: Request, res: Response) => {
-        try {
 
+        try {
             const movies = await getMoviesParsedService();
-         /*    const {
-                query: {filter, value}
-            } = req;
-            if (filter && value) return res.send(
-                movies.filter((movie) => movie.genero[filter].includes(value))
-            ) */
+            if (req.query.genero) {
+                if(!genders.includes(req.query.genero as string)) {
+                     res.sendStatus(404);
+                     return
+                }
+                const filteredMovie = movies.filter(movie => hasGenero(req.query.genero as string, movie.genero));
+                res.status(200).send(filteredMovie);
+                return;
+            }
              res.status(200).send(movies);
         } catch (error) {
              res.sendStatus(500);
