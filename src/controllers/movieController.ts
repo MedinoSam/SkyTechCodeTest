3e5ddb3642e4c2180de1a2af4a8a358604c56290
medinoSam <samuelejosuetutorial@gmail.com>
2025-07-06 01:45:44 -0300

    feat: dividing controller responsibility

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -1,8 +1,19 @@
-import { Router } from "express";
-import { getMoviesParsed } from "../services/movieService";
+import { Request, Response} from "express";
+import { getMoviesParsedService } from "../services/movieService";
+import { query } from "express-validator";
 
-const movieRouter = Router();
+export const getMoviesParsedController =  async (req: Request, res: Response) => {
+        try {
 
-movieRouter.get('/', getMoviesParsed)
-
-export default movieRouter;
+            const movies = await getMoviesParsedService();
+         /*    const {
+                query: {filter, value}
+            } = req;
+            if (filter && value) return res.send(
+                movies.filter((movie) => movie.genero[filter].includes(value))
+            ) */
+             res.status(200).send(movies);
+        } catch (error) {
+             res.sendStatus(500);
+        }
+}
