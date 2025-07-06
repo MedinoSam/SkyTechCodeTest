c3eed840b3048b007dfea9483499d4bad916c72f
medinoSam <samuelejosuetutorial@gmail.com>
2025-07-06 01:44:34 -0300

    feat: create movie router

---
+++ src/routes/movieRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { getMoviesParsedController } from "../controllers/movieController";
+const movieRouter = Router();
+
+movieRouter.get('/api/filmes', getMoviesParsedController)
+
+export default movieRouter;
